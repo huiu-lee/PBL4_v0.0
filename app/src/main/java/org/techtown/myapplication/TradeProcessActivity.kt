89aff5c87ab6d9ceaf0eaa7f1736447d70cc969b
huiu <huiu1204@naver.com>
2021-11-15 15:46:50 +0900

    Test Commit

---
+++ app/src/main/java/org/techtown/myapplication/TradeProcessActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TradeProcessActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trade_process)
+    }
+}
